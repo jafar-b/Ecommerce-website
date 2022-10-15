f4b228615c80524ada70244c0956b31181daacb2
jafar-b <beldarjafar.com>
2022-10-15 09:59:36 +0530

    resolved backend signup endpoint error(data was not getting saved to db)

--- backend/api/Index.js
+++ backend/api/Index.js
@@ -15,54 +15,38 @@ app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-
-
-
-
-
 app.post("/SignUp", (req, res) => {
   const { name, email, pass, pass2 } = req.body;
-  const user = new usermodel({ name, email, pass, pass2 });
-
 
   if (!name || !email || !pass || !pass2) {
-    res.status(400).json({ "error": "please enter fields proeprly" });
-  }
+    res.status(400).json({ error: "please enter fields proeprly" });
+  } else {
+    usermodel
+      .findOne({ email: email })
+      .then((userExist) => {
+        if (userExist) {
+          return res.status(422).json({ message: "user already exists" });
+        }
+        const user = new usermodel({ name, email, pass, pass2 });
 
-else{
-
-  usermodel
-  .findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        res.status(201).json({ "message": "USER EXISTS" });
-       
-      }
-    }).then(()=>{
-
-      
-      
-  user
-    .save()
-    .then(() => {
-      res.status(200).json({ "success": " signup data saved successfully" });
-    })
-    .catch((err) =>
-      res.status(500).json({ "signup error hai bhai": "data save nhi hua" })
-    );
-  }).catch((err)=>{res.json({"error":"error hai bhai"})});
+        user
+          .save()
+          .then(() => {
+            return res
+              .status(200)
+              .json({ success: " signup data saved successfully" });
+          })
+          .catch((err) => console.log("error data not saved"));
+      })
+      .catch((err) => {
+        console.log("error hai bhai pure code me");
+      });
   }
 });
 
-
-
-
-
-
-
 app.get("/Login", (req, res) => {
   const { email, pass } = req.body;
-  console.log(email); 
+  console.log(email);
   console.log(pass);
   res.json({ message: req.body });
 });
@@ -74,6 +58,5 @@ app.get("/api/sample", (req, res) => {
 
 app.listen(port, () => {
   console.log("server listening at port ", port);
-  mongoose
-    .connect(url).then(() => console.log("connection successfull"));
+  mongoose.connect(url).then(() => console.log("connection successfull"));
 });
